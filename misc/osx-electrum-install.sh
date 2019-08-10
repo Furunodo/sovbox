81eb18524ca139dfb45df04ef84193c053dd7b25
Furunodo <52054596+Furunodo@users.noreply.github.com>
2019-08-10 18:18:39 +0200

    electrum checksum check in osx installer

--- misc/osx-electrum-install.sh
+++ misc/osx-electrum-install.sh
@@ -2,7 +2,10 @@
 # @todo check if this can become part of brew?
 
 # @todo VERSION should be derived from `curl https://downlad.electrum.org/` 
-VERSION=3.3.7
+VERSION=3.3.8
+
+mkdir -p /tmp/electrum
+cd /tmp/electrum
 
 wget https://download.electrum.org/$VERSION/electrum-$VERSION.dmg
 wget https://download.electrum.org/$VERSION/electrum-$VERSION.dmg.asc
@@ -14,9 +17,15 @@ wget https://download.electrum.org/$VERSION/electrum-$VERSION.dmg.asc
 gpg --keyserver keys.gnupg.net --recv-keys 6694D8DE7BE8EE5631BED9502BD5824B7F9470E6
 
 gpg --verify electrum-$VERSION.dmg.asc electrum-$VERSION.dmg
+RESULT=$?
 
-open electrum-$VERSION.dmg
-
+if [ $RESULT -eq 0 ]; then
+    echo "All seems good"
+    echo "Note that the 'not certified with a trusted signature' error can be resolved by joining GPG signing parties."
+    open electrum-$VERSION.dmg
+else
+    echo "WARNING, electrum-$VERSION.dmg doesn't match the author's signatore"
+fi
 
 # @todo
 #  - help with setting up tor on osx?  brew install tor && brew services start tor
